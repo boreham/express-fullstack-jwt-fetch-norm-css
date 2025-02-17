8c88730eda24b6297dbc99a486a2ec381ea5a862
Kong Boreham <duohold@gmail.com>
2025-02-17 08:42:44 +0300

    Update env

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://kong:secret@192.168.8.166:5432/jwt',
+  connectionString: process.env.DATABASE_URL || 'postgresql://user:password@localhost:5432/database',
 });
 
 export default pool;
